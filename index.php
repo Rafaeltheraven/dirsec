c32442b62d5d48157661b8ebfd427e95fb6e64fc
Hong Shen Zhou <hong.shenzhou@gmail.com>
2014-11-30 16:43:07 +0200

    Started working on actual content

--- index.php
+++ index.php
@@ -56,6 +56,25 @@
         
         <!-- Begin main website body -->
         <div class="content body" id="body-background">
+            <!-- Placeholder for now... -->
+        </div>
+        
+        <!-- Begin seperator section (bottom) -->
+        <div class="seperator content" id="bottom-seperator">
+            
+            <!-- Container for an email address (contact info) -->
+            <div class="content" id="contactinfo">
+                <!-- Will use recaptcha mailhide to protect spam -->
+                <a href="http://www.google.com/recaptcha/mailhide/d?k=01g-3xbBaAqHRpIK6ZancRVA==&amp;c=1kevnjVi_aJfQ-xQYx1ydw-tuIeSQpGAbaRTxEGMH0E=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501g-3xbBaAqHRpIK6ZancRVA\75\75\46c\0751kevnjVi_aJfQ-xQYx1ydw-tuIeSQpGAbaRTxEGMH0E\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">h...</a>@gmail.com
+            </div>
+        </div>
+        
+        <!-- Begin footer with maps list -->
+        <div class="content footer" id="footer">
+            <h2 id="maplist-header">
+                Maps:
+            </h2>
+            
         </div>
     </div>
   </body>
